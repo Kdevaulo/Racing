3cbbe17bbc0e0dc0ce11a11173532be4c0830f8f
Kdevaulo <kdevaulo@gmail.com>
2025-10-29 18:07:12 +0500

    Default network behaviour test

---
+++ Assets/Scripts/NetworkTransformTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Unity.Netcode;
+
+using UnityEngine;
+
+namespace Kdevaulo.Racing
+{
+    public class NetworkTransformTest : NetworkBehaviour
+    {
+        void Update()
+        {
+            if (IsServer)
+            {
+                float theta = Time.time;
+                transform.position = new Vector3((float) Math.Cos(theta), 0.0f, (float) Math.Sin(theta));
+            }
+        }
+    }
+}
